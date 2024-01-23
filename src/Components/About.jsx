f0597836fb4ed8453d5a077582a08770611fb9f6
Ricka <123356733+Jayricka@users.noreply.github.com>
2024-01-23 15:40:42 +0000

    components pop

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,33 +10,33 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/background.jpg";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "orange and white abstract painting";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "Hello there! I'm not just a software engineer; I'm a passionate architect of digital solutions. Rooted in a strong academic background and hands-on professional experience, I bring a solid foundation in computer science and software engineering to the table. My expertise lies in crafting robust and scalable backend systems, where every line of code is a testament to precision and innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Web development",
+  "Back-End Programming",
+  "Database Management",
+  "API Design",
+  "System Architecture",
+  "Software Optimization",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "What drives me? I'm deeply passionate about leveraging technology to create positive change in the world. I champion diversity and inclusion in tech, believing that innovation thrives when perspectives from all walks of life converge. My mission is to build not just functional backend systems but to pave the way for a more inclusive and accessible digital future.";
 
 const About = () => {
   return (
